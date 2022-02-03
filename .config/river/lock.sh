9456c9b3c270ad0fc70c89b9a3ee6da5575af49f
kugelblitz2 <kugelblitz3@outlook.com>
2022-02-03 09:17:53 -0500

    Added Swaylock

---
+++ .config/river/lock.sh
@@ -0,0 +1,6 @@
+swaylock --screenshots --clock --indicator-idle-visible --indicator-radius 100  --indicator-thickness 7 \
+--ring-color 455a64 --key-hl-color 32cd32 --text-color e6e6fa --line-color 00000000 --inside-color 00000088 --separator-color 00000000 \
+--ring-clear-color 455a64 --bs-hl-color dc143c --text-clear-color e6e6fa --line-clear-color 00000000 --inside-clear-color 00000088 \
+--ring-wrong-color dc143c --text-wrong-color e6e6fa --line-wrong-color 00000000 --inside-wrong-color 00000088 \
+--ring-ver-color 00ced1 --text-ver-color e6e6fa --line-ver-color 00000000 --inside-ver-color 00000088 \
+--fade-in 0.1 --effect-scale 0.5 --effect-blur 7x3 --effect-scale 2 --effect-vignette 0.5:0.5 --timestr "%-H:%M %p" --datestr "%B %-d %Y"
