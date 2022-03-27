93601b35e466c93ddddfca998c77f114fce08647
kugelblitz2 <kugelblitz3@outlook.com>
2022-03-27 06:10:16 -0400

    added kanji file and neofetch config

---
+++ .config/kanji.sh
@@ -0,0 +1,27 @@
+declare -A nihon
+
+nihon['tokyo']='東京'
+nihon['shinjuku']='新宿'
+nihon['shibuya']='渋谷'
+nihon['suginami']='杉並'
+nihon['chiyoda']='千代田'
+nihon['hachioji']='八王子'
+
+nihon['kyoto']='京都'
+nihon['osaka']='大阪'
+nihon['nara']='奈良'
+nihon['kobe']='神戸'
+
+nihon['chiba']='千葉'
+nihon['sendai']='仙台'
+nihon['nagoya']='名古屋'
+nihon['akita']='秋田'
+nihon['aomori']='青森'
+
+nihon['kumamoto']='熊本'
+nihon['nagasaki']='長崎'
+nihon['sasebo']='佐世保'
+nihon['kagoshima']='鹿児島'
+
+hostname=$(hostname -s)
+NIHON=${nihon[$hostname]}
